9429611ae648b82eedc97eb8c56e6f06a57bc8fb
ShTair <git@vsh.jp>
2019-10-29 16:20:34 +0900

    証明書の取得を実装
    失敗する

--- CertificatesChecker/Program.cs
+++ CertificatesChecker/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace CertificatesChecker
@@ -12,7 +14,26 @@ namespace CertificatesChecker
 
         private static async Task Run()
         {
+            var certificate = await GetCertificateAsync("https://www.google.co.jp/");
+        }
 
+        private static async Task<X509Certificate2> GetCertificateAsync(string uri)
+        {
+            var tcs = new TaskCompletionSource<X509Certificate2>();
+
+            bool ServerCertificateCustomValidationCallback(HttpRequestMessage message, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors errors)
+            {
+                tcs.TrySetResult(certificate);
+                return true;
+            }
+
+            var handler = new HttpClientHandler { ServerCertificateCustomValidationCallback = ServerCertificateCustomValidationCallback };
+            using (var hc = new HttpClient(handler))
+            {
+                await hc.GetAsync(uri);
+            }
+
+            return await tcs.Task;
         }
     }
 }
