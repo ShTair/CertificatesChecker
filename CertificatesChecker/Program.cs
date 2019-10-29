d91f96d38551d0d3636c3c7e95123d88f2c2da00
ShTair <git@vsh.jp>
2019-10-29 16:04:46 +0900

    プロジェクト ファイルを追加します。

---
+++ CertificatesChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CertificatesChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+
+        }
+    }
+}
