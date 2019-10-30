dfd6013ec75a7c299cf0a7e9a150a2072dde0500
ShTair <git@vsh.jp>
2019-10-30 11:34:15 +0900

    ログの表示を実装

--- CertificatesChecker/Program.cs
+++ CertificatesChecker/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -21,35 +22,67 @@ namespace CertificatesChecker
             var settings = JsonConvert.DeserializeObject<List<string>>(settingsJson);
             if (settings == null) return;
 
-            foreach (var setting in settings)
+            var now = DateTime.Now;
+            var logLock = new object();
+            var datas = await Task.WhenAll(settings.Select(async uri =>
             {
-                using (var certificate = await GetCertificateAsync(setting))
+                var certificate = await GetCertificateAsync(uri);
+
+                lock (logLock)
                 {
                     Console.WriteLine();
-                    Console.WriteLine(setting);
-                    Console.WriteLine(certificate.Subject);
-                    Console.WriteLine($"{certificate.NotBefore:yyyy/MM/dd HH:mm} -> {certificate.NotAfter:yyyy/MM/dd HH:mm}");
+                    Console.WriteLine($"Uri    : ({(DateTime.Now - now).TotalSeconds:0.00}秒){uri}");
+                    Console.WriteLine($"Subject: {certificate.Subject}");
+                    Console.WriteLine($"Issuer : {certificate.Issuer}");
+                    Console.WriteLine($"Valid  : [{certificate.NotBefore:yyyy/MM/dd HH:mm:ss}] -> [{certificate.NotAfter:yyyy/MM/dd HH:mm:ss}]");
                 }
+
+                return (uri, certificate);
+            }));
+
+            Console.WriteLine();
+
+            now = DateTime.Now;
+            var limit = now.AddDays(-30);
+            foreach (var (uri, certificate) in datas.OrderBy(t => t.certificate.NotAfter))
+            {
+                var span = certificate.NotAfter - now;
+                certificate.Dispose();
+
+                var message = "期限切れです！！";
+                if (span > TimeSpan.Zero)
+                {
+                    var days = span.TotalDays;
+                    message = $"残り{days:0}日";
+                }
+
+                Console.WriteLine($"{message} {uri}");
             }
         }
 
         private static async Task<X509Certificate2> GetCertificateAsync(string uri)
         {
             var tcs = new TaskCompletionSource<X509Certificate2>();
-
-            var handler = new HttpClientHandler
+            _ = Task.Run(async () =>
             {
-                ServerCertificateCustomValidationCallback = (_1, c, _2, _3) =>
+                var handler = new HttpClientHandler
                 {
-                    tcs.TrySetResult(new X509Certificate2(c.RawData));
-                    return true;
-                },
-            };
+                    ServerCertificateCustomValidationCallback = (_1, c, _2, _3) =>
+                    {
+                        tcs.TrySetResult(new X509Certificate2(c.RawData));
+                        return false;
+                    },
+                };
 
-            using (var hc = new HttpClient(handler))
-            {
-                await hc.GetAsync(uri);
-            }
+                try
+                {
+                    using (var hc = new HttpClient(handler))
+                    {
+                        await hc.GetAsync(uri);
+                    }
+                }
+                catch { }
+            });
 
             return await tcs.Task;
         }
