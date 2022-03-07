8ed68333123b302833a71956a9139ce0e3b3aabd
ShTair <git@vsh.jp>
2022-03-07 09:53:15 +0900

    net6に変更

--- CertificatesChecker/Program.cs
+++ CertificatesChecker/Program.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace CertificatesChecker
 {
